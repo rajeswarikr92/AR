70eca629385ffeccf88ebc2b4fc00c095316cdb6
Raji <rajikr@gmail.com>
2020-06-18 10:52:55 +0530

    Added AR fragment

--- app/src/main/java/com/example/ar/MainActivity.java
+++ app/src/main/java/com/example/ar/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.ar.sceneform.ux.ArFragment;
+
 public class MainActivity extends AppCompatActivity {
+    private ArFragment arCoreFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        arCoreFragment = (ArFragment)
+        getSupportFragmentManager().findFragmentById(R.id.sceneform_fragment);
     }
 }
